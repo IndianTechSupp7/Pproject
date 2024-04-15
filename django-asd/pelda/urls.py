36925c588a5cff2eef7b667a039cefb2da5c58c5
IndianTechSupp <90865605+Vasas7@users.noreply.github.com>
2024-04-15 20:34:05 +0200

    Add files via upload

---
+++ django-asd/pelda/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.login_user, name="login"),
+    path('home/', views.home, name="home"),
+    path('register/', views.register, name="register"),
+    path('logout/', views.logout_user, name="logout"),
+    path('kapcsolat/', views.kapcsolat, name="kapcsolat"),
+    path('rolunk/', views.rolunk, name="rolunk"),
+    path('feladat/', views.feladat, name="feladat"),
+    path('terulet/', views.terulet, name="terulet"),
+]
